d12860c960e60bfbc30e447b1913938123a12351
Jacob Kupersmith <jake@jkup.org>
2024-03-02 02:24:46 -0500

    it aint pretty, but it's faster..

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Just having some fun trying out [1BRC](https://github.com/gunnarmorling/1brc) using Golang.
 
+_2024-03-01: Using file.Read was a lot faster than bufio.Scanner (~1s vs ~11s) reading the file in a single thread.  This version uses a pool of 16 worker goroutines to read and process the file._
 _2024-02-28: I created a baseline implementation, it produced the correct output in 1m0.535s on my home workstation.  baseline java implementation took 2m6.798s for comparision._
 
 
@@ -12,3 +13,4 @@ _2024-02-28: I created a baseline implementation, it produced the correct output
 | 1brc java baseline | 2m6.798s |
 | v0.9 | 1m18.948s |
 | v1.0 | 1m0.535s |
+| v1.1 | 0m28.678s |
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math"
 	"os"
 	"sort"
+	"sync"
 )
+
+const BUF_SIZE = 1048576
 
 type mma struct {
 	Min   int16
@@ -15,7 +17,12 @@ type mma struct {
 	Count int
 }
 
-func parseLine(s string) (stationName string, measurement int16) {
+type row struct {
+	StationName string
+	Measurement int16
+}
+
+func parseLine(s []byte) (r row) {
 	var i int = 0
 
 	for i = 0; i < len(s); i++ {
@@ -23,7 +30,7 @@ func parseLine(s string) (stationName string, measurement int16) {
 			break
 		}
 	}
-	stationName = s[:i]
+	r.StationName = string(s[:i])
 
 	neg := false
 	for i++; i < len(s); i++ {
@@ -32,47 +39,129 @@ func parseLine(s string) (stationName string, measurement int16) {
 			neg = true
 		case '.':
 		default:
-			measurement = (measurement * 10) + int16(s[i]-'0')
+			r.Measurement = (r.Measurement * 10) + int16(s[i]-'0')
 		}
 	}
 	if neg {
-		measurement = -measurement
+		r.Measurement = -r.Measurement
 	}
 	return
 }
 
 func main() {
-	var stations []string
+	var wg sync.WaitGroup
+	var readlock sync.Mutex
+	var maplock sync.RWMutex
 	results := make(map[string]*mma)
+
+	eof := false
+	pbuf := make([]byte, BUF_SIZE)
+	pi := 0
 
 	fh, _ := os.Open("measurements.txt")
 	defer fh.Close()
 
-	scanner := bufio.NewScanner(fh)
-	for scanner.Scan() {
-		stationName, measurement := parseLine(scanner.Text())
-		v, ok := results[stationName]
-		if !ok {
-			stations = append(stations, stationName)
-			results[stationName] = &mma{
-				Min:   measurement,
-				Max:   measurement,
-				Sum:   int(measurement),
-				Count: 1,
+	for workerId := 0; workerId < 16; workerId++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			buf := make([]byte, BUF_SIZE)
+			for !eof {
+				readlock.Lock()
+				n, _ := fh.Read(buf)
+				if n == 0 {
+					eof = true
+					readlock.Unlock()
+					break
+				}
+
+				// Copy partial values from beginning and end of buf somewhere for later processing..
+				var firstnl int
+				for firstnl = 0; firstnl < n; firstnl++ {
+					if buf[firstnl] == '\n' {
+						break
+					}
+				}
+				copy(pbuf[pi:], buf[:firstnl+1])
+				pi += firstnl + 1
+
+				var lastnl int
+				for lastnl = n - 1; lastnl >= 0; lastnl-- {
+					if buf[lastnl] == '\n' {
+						break
+					}
+				}
+				copy(pbuf[pi:], buf[lastnl+1:n])
+				pi += n - lastnl - 1
+
+				readlock.Unlock()
+
+				for i, j := firstnl+1, firstnl+1; j <= lastnl; j++ {
+					if buf[j] == '\n' {
+						r := parseLine(buf[i:j])
+						i = j + 1
+						maplock.RLock()
+						v, ok := results[r.StationName]
+						maplock.RUnlock()
+						if !ok {
+							maplock.Lock()
+							results[r.StationName] = &mma{
+								Min:   r.Measurement,
+								Max:   r.Measurement,
+								Sum:   int(r.Measurement),
+								Count: 1,
+							}
+							maplock.Unlock()
+							continue
+						} else {
+							if r.Measurement < v.Min {
+								v.Min = r.Measurement
+							}
+							if r.Measurement > v.Max {
+								v.Max = r.Measurement
+							}
+							v.Sum += int(r.Measurement)
+							v.Count++
+						}
+					}
+				}
 			}
-			continue
-		} else {
-			if measurement < v.Min {
-				v.Min = measurement
+		}()
+	}
+
+	wg.Wait()
+	for i, j := 0, 0; j < pi; j++ {
+		if pbuf[j] == '\n' {
+			r := parseLine(pbuf[i:j])
+			i = j + 1
+			v, ok := results[r.StationName]
+			if !ok {
+				results[r.StationName] = &mma{
+					Min:   r.Measurement,
+					Max:   r.Measurement,
+					Sum:   int(r.Measurement),
+					Count: 1,
+				}
+				continue
+			} else {
+				if r.Measurement < v.Min {
+					v.Min = r.Measurement
+				}
+				if r.Measurement > v.Max {
+					v.Max = r.Measurement
+				}
+				v.Sum += int(r.Measurement)
+				v.Count++
 			}
-			if measurement > v.Max {
-				v.Max = measurement
-			}
-			v.Sum += int(measurement)
-			v.Count++
 		}
 	}
 
+	stations := make([]string, len(results))
+	i := 0
+	for k := range results {
+		stations[i] = k
+		i++
+	}
 	sort.Strings(stations)
 	prefix := "{"
 	for _, stationName := range stations {
